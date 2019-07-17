e5ab623ddffdb18e54a87b8f6d54cd88d3e75f2a
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2019-07-17 19:38:30 -0300

    Missing App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <div id="app">
-    <!-- Offline status should ask Onboarding to base App & gathering wallet information -->
-    <div v-if="isOffline">
-      <Onboarding>
-      </Onboarding>
-    </div>
+    <AeMain>
+      <img alt="AE-logo" src="assets/ae2x.png" />
+        <div>
+          <ae-text fill="primary" face="sans-l">Aternity Universe</ae-text>
+        </div>
+      <ae-text fill="secondary" face="sans-base">Developers Conference</ae-text>
+      <router-view/>
+    </AeMain>
   </div>
 </template>
 
 <script>
+import {
+  AeMain,
+  AeText
+} from "@aeternity/aepp-components"; 
+
 import "@aeternity/aepp-components/dist/aepp.global.css";
 import "@aeternity/aepp-components/dist/aepp.components.css";
-import Onboarding from "./components/Onboarding.vue";
 
 export default {
-  name: "app",
-  components: {
-    Onboarding
-  },
-  data() {
-    return {
-      isOffline: true
-    };
-  }
-};
+  name: "App",
+  components: { AeMain, AeText }
+
+}
 </script>
+
+<style>
+#app {
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
