3593951f00d154c6cf8663d2f63cf34c0dc7b8c1
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2019-07-23 16:13:31 -0300

    Deposit WIP

--- client/src/views/Deposit.vue
+++ client/src/views/Deposit.vue
@@ -1,39 +1,61 @@
 <template>
-    <div class="deposit" >
-     <div v-if="initialDeposit">
-       <AeText>To open a channel with our Point of Sale services an acquire
-        venue amenities, goods and services, 
-        please enter an amount of AE to deposit.  This amount can be 
-        used immediately. Unspent tokens will be returned to your wallet xxx
-        </AeText>
-        <AeText face="sans-s">You can add more tokens later</AeText>
-     </div>
-     <div v-else>
-       <AeText>How many tokens do you want to deposit in the PoS channel?</AeText>
-     </div>
-     <ae-amount-input
-          placeholder="0.00"
-          v-model="depositAmount"
-          :units="[
+  <div class="deposit">
+    <div v-if="initialDeposit">
+      <AeText>
+        To open a channel with our Point of Sale services an acquire
+        venue amenities, goods and services,
+        please enter an amount of AE to deposit. This amount can be
+        used immediately. Unspent tokens will be returned to your wallet when the channel closes.
+      </AeText>
+      <AeText face="sans-s">You can add more tokens later</AeText>
+    </div>
+    <div v-else>
+      <AeText>How many tokens do you want to deposit in the PoS channel?</AeText>
+    </div>
+    <ae-amount-input
+      placeholder="0.00"
+      v-model="depositInput"
+      :units="[
             { symbol: 'AE', name: 'æternity' }
           ]"
-        />
-      <AeButton face="round" fill="primary" extend @click="deposit()">Deposit</AeButton>
+      @input="onAmountInput"
+      v-bind:visible="isQueryingBalance"
+    />
+    <div v-if="isQueryingBalance">
+      <AeText>Please wait while Checking your account balance</AeText>
+      <AeLoader />
     </div>
+    <div v-else>
+      <AeButton
+        face="round"
+        fill="primary"
+        extend
+        @click="deposit()"
+        :disabled="depositInput.amount <= 0 || isInError || isQueryingBalance"
+      >Deposit</AeButton>
+    </div>
+  </div>
 </template>
 
 <script>
+const STATUS_USER_INPUT = 0,
+  STATUS_QUERY_BALANCE = 1,
+  STATUS_ERROR = 2;
+
 import {
   AeText,
   AeAmountInput,
+  AeLoader,
   AeButton
 } from "@aeternity/aepp-components";
+import BigNumber from "bignumber.js";
 
 export default {
   name: "Deposit",
   components: {
     AeButton,
     AeText,
+    AeLoader,
     AeAmountInput
   },
   props: {
@@ -41,18 +63,64 @@ export default {
   },
   data() {
     return {
-      depositAmount: {
+      viewState: STATUS_USER_INPUT,
+      errorReason: null,
+      depositInput: {
         amount: "1",
-        symbol: 'AE'
+        symbol: "AE"
       }
     };
   },
   computed: {
-   
+    balance() {
+      return this.$store.state.balance;
+    },
+    isWaitingUserInput() {
+      return this.viewState == STATUS_USER_INPUT;
+    },
+    isQueryingBalance() {
+      return this.viewState == STATUS_QUERY_BALANCE;
+    },
+    isInError() {
+      return this.viewState == STATUS_ERROR;
+    }
   },
   methods: {
-    deposit() {
-      this.$router.push({ name: 'confirm-tx', params: { txKind: this.initialDeposit ? 'initial-deposit' : 'deposit' }});
+    async deposit() {
+      console.log("Setting status to STATUS_QUERY_BALANCE");
+      this.viewState = STATUS_QUERY_BALANCE;
+      try {
+        await this.$store.dispatch("getCurrentBalance");
+
+        const balanceBN = new BigNumber(this.$store.state.balance);
+        const inputBN = new BigNumber(this.depositInput.amount);
+
+        // TODO!  Consider fees (approximate? )
+
+        console.log("balance: " + this.$store.state.balance);
+
+        if (balanceBN.lt(inputBN.multipliedBy(10**18))) {
+          this.errorReason = "Not enough funds.";
+          this.viewState = STATUS_ERROR;
+        } else {
+          this.viewState = STATUS_USER_INPUT;
+          this.$router.push({
+            name: "confirm-tx",
+            params: {
+              txKind: this.initialDeposit ? "initial-deposit" : "deposit"
+            }
+          });
+        }
+      } catch (e) {
+        console.log(
+          "Setting status to STATUS_ERROR with Text: " + e.toString()
+        );
+        this.errorReason = e.toString();
+        this.viewState = STATUS_ERROR;
+      }
+    },
+    onAmountInput(v) {
+      this.depositInput = v;
     }
   }
 };
