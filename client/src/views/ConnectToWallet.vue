51ce822f8be55ef4ef1235c86dec9ad6f78df5ff
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2019-07-31 12:29:54 -0300

    Support for application roles

--- client/src/views/ConnectToWallet.vue
+++ client/src/views/ConnectToWallet.vue
@@ -17,7 +17,7 @@
 
 <script>
 /* eslint-disable no-console */
-import aeternity from '../controllers/aeternity.js'
+import aeternity from "../controllers/aeternity.js";
 import {
   AeText,
   AeButton,
@@ -25,7 +25,7 @@ import {
   AeLoader
 } from "@aeternity/aepp-components";
 
-const STATUS_OFFLINE = 0, 
+const STATUS_OFFLINE = 0,
   STATUS_INIT = 0,
   STATUS_CONNECTING = 1,
   STATUS_CONNECTED = 2,
@@ -46,11 +46,21 @@ export default {
     };
   },
   computed: {
-    isOffline() { return this.status == STATUS_OFFLINE },
-    isAtInitialState() { return this.status == STATUS_INIT },
-    isConnecting() { return this.status == STATUS_CONNECTING },
-    isConnected() { return this.status == STATUS_CONNECTED },
-    isAtError() { return this.status == STATUS_ERROR }
+    isOffline() {
+      return this.status == STATUS_OFFLINE;
+    },
+    isAtInitialState() {
+      return this.status == STATUS_INIT;
+    },
+    isConnecting() {
+      return this.status == STATUS_CONNECTING;
+    },
+    isConnected() {
+      return this.status == STATUS_CONNECTED;
+    },
+    isAtError() {
+      return this.status == STATUS_ERROR;
+    }
   },
   methods: {
     async connectToBaseApp() {
@@ -59,23 +69,42 @@ export default {
         const connectStatus = await aeternity.connectToBaseApp();
         console.log(connectStatus);
         if (connectStatus.status) {
-          this.$store.commit('setAeObject', aeternity);
+          this.$store.commit("setAeObject", aeternity);
           this.status = STATUS_CONNECTED;
-          this.$router.push( { name: 'scanqr', params: { subview: 'onboarding'}});
+          this.$router.push({
+            name: "scanqr",
+            params: { subview: "onboarding" }
+          });
         } else {
           this.setError(connectStatus.error.toString());
         }
-      }
-      catch(e) {
+      } catch (e) {
         this.setError(e.toString());
       }
     },
     setError(errorText) {
-      this.$router.push( { name: 'error', 
-        params: { errorTitle: 'We could not connect to your wallet', 
-          errorDescription: errorText, 
+      this.$router.push({
+        name: "error",
+        params: {
+          errorTitle: "We could not connect to your wallet",
+          errorDescription: errorText,
           retryCancel: false
-        }})
+        }
+      });
+    }
+  },
+  mounted() {
+    // Preflight checks
+
+    if (process.env.VUE_APP_ROLE === "merchant") {
+      console.warn("Booting application with role:  MERCHANT");
+    } else if (process.env.VUE_APP_ROLE === "client") {
+      console.warn("Booting application with role:  CLIENT");
+    } else {
+      console.error("Cannot find application role in VUE_APP_ROLE variable");
+      this.setError(
+        "Application cannot start. Set proper application role to either MERCHANT or CLIENT"
+      );
     }
   }
 };
