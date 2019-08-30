ff0eb727b2afe020902b9ee6ee4e3010354b7bf6
Franco Scucchiero <scucchiero@gmail.com>
2019-08-30 13:01:53 -0300

    Placed description and amount in the same line. (Transaction Panel)

--- client/src/views/ConfirmTx.vue
+++ client/src/views/ConfirmTx.vue
@@ -13,7 +13,7 @@
       :fee="feeAE"
     />
     <ViewButtonSection
-      :buttons="[{name:'Confirm', action: confirm},{name:'Cancel', action: cancel, fill:'neutral'}]"
+      :buttons="[{name:'Cancel', action: cancel, fill:'neutral'}, {name:'Confirm', action: confirm}]"
     />
   </b-container>
 </template>
