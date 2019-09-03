d7c4fe1c81fd607784e8ebdd480e1cd8c19668a3
fabimar <fabian.marino@coinfabrik.com>
2019-09-03 13:56:47 -0300

    History "Add Tx" button > Circle

--- client/src/views/History.vue
+++ client/src/views/History.vue
@@ -123,11 +123,11 @@
           </div>
         </b-modal>
         <AeButton
-          face="round"
+          face="icon"
           fill="primary"
           @click="addItems(history.length)"
         >
-          +
+          <AeIcon name="plus" />
         </AeButton>
       </AeList>
     </div>
